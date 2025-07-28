f8844d1fb44f31b9e2e9b2a0f27e679a689ddef1
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-07-28 17:34:05 +0530

    Edit frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
+//import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css'
-import LandingPage from './components/LandingPage';
+import Home from './pages/Home';
+import Header from '../src/components/Header';
+import About from '../src/pages/About';
+import Services from '../src/pages/Services';
+import Contact from '../src/pages/Contact';
+import Footer from './components/Footer';
 
 
 function App() {
-  return <LandingPage />
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* Add more routes as needed */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App
